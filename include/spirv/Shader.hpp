0387d9d1fdbc36e7b60a2ec4fedf575fb7075757
maxwelliverson <54956548+maxwelliverson@users.noreply.github.com>
2021-02-25 13:29:41 -0800

    Added SpirV skeleton files

---
+++ include/spirv/Shader.hpp
@@ -0,0 +1,32 @@
+//
+// Created by Maxwell on 2021-01-27.
+//
+
+#ifndef VALKYRIE_SPIRV_SHADER_HPP
+#define VALKYRIE_SPIRV_SHADER_HPP
+
+#include <spirv/Enums.hpp>
+
+namespace valkyrie::Spv{
+
+  class ShaderGraph{};
+  class ShaderGraphNode{};
+  class ShaderInInterface{};
+  class ShaderOutInterface{};
+
+
+  class VertexShaderInInterface{};
+  class VertexShaderOutInterface{};
+  class TCShaderInInterface{};
+  class TCShaderOutInterface{};
+  class GeometryShaderInInterface{};
+  class GeometryShaderOutInterface{};
+  class FragmentShaderOutInterface{};
+  class FragmentShaderInInterface{};
+
+
+
+}
+
+
+#endif //VALKYRIE_SPIRV_SHADER_HPP
