571ce8b092ddeb06163c8c226b617db824d444c6
Maxwell Iverson <maxwelliverson@gmail.com>
2021-05-06 13:43:25 -0700

    made switch from code::PascalCase to code::snake_case in `status/system.cpp`

--- src/valkyrie/status/system.cpp
+++ src/valkyrie/status/system.cpp
@@ -16,83 +16,83 @@ namespace {
 
   inline code translatePosixCodeToGeneric(const int posixError) noexcept {
     switch(posixError) {
-      case EACCES: return code::PermissionDenied;
+      case EACCES: return code::permission_denied;
       case EADDRINUSE:
-      case EADDRNOTAVAIL: return code::AddressNotAvailable;
-      case EAGAIN:  return code::ResourceUnavailable;
-      case EALREADY: return code::InProgress;
-      case EBADF: return code::ResourceNotFound;
-      case EBADMSG: return code::InvalidResource;
-      case EBUSY: return code::Busy;
-      case ECANCELED: return code::Cancelled;
-      case ECHILD: return code::ResourceNotFound;
-      case ECONNABORTED: return code::ConnectionAborted;
-      case ECONNREFUSED: return code::ConnectionRefused;
-      case ECONNRESET:   return code::ConnectionReset;
-      case EDEADLK:      return code::Deadlock;
+      case EADDRNOTAVAIL: return code::address_not_available;
+      case EAGAIN:  return code::resource_unavailable;
+      case EALREADY: return code::in_progress;
+      case EBADF: return code::resource_not_found;
+      case EBADMSG: return code::invalid_resource;
+      case EBUSY: return code::busy;
+      case ECANCELED: return code::cancelled;
+      case ECHILD: return code::resource_not_found;
+      case ECONNABORTED: return code::connection_aborted;
+      case ECONNREFUSED: return code::connection_refused;
+      case ECONNRESET:   return code::connection_reset;
+      case EDEADLK:      return code::deadlock;
         //**EDESTADDRREQ    Destination address required (POSIX.1-2001).
-      case EDOM: return code::OutOfDomain;
-      case EEXIST: return code::ResourceAlreadyExists;
-      case EFAULT: return code::BadAddress;
+      case EDOM: return code::out_of_domain;
+      case EEXIST: return code::resource_already_exists;
+      case EFAULT: return code::bad_address;
 
         //**EHOSTUNREACH    Host is unreachable (POSIX.1-2001)
         //**EIDRM           Identifier removed (POSIX.1-2001)
-      case EILSEQ: return code::InvalidFormat;
-      case EINPROGRESS: return code::InProgress;
-      case EINTR:  return code::Interrupted;
-      case EINVAL: return code::InvalidArgument;
-      case EIO:    return code::IOError;
-      case EISCONN: return code::AlreadyConnected;
-      case EISDIR: return code::ResourceUnexpectedType;
-      case ELOOP:  return code::RecursionDepth;
+      case EILSEQ: return code::invalid_format;
+      case EINPROGRESS: return code::in_progress;
+      case EINTR:  return code::interrupted;
+      case EINVAL: return code::invalid_argument;
+      case EIO:    return code::io_error;
+      case EISCONN: return code::already_connected;
+      case EISDIR: return code::resource_unexpected_type;
+      case ELOOP:  return code::recursion_depth;
       case EMFILE:
-      case EMLINK: return code::ResourceLimitReached;
+      case EMLINK: return code::resource_limit_reached;
       case EFBIG:
       case EMSGSIZE:
-      case ENAMETOOLONG: return code::ResourceTooLarge;
+      case ENAMETOOLONG: return code::resource_too_large;
       case ENETDOWN:
       case ENETRESET:
-      case ENETUNREACH: return code::NetworkUnavailable;
-      case ENFILE: return code::ResourceLimitReached;
-      case ENOBUFS: return code::InsufficientSize;
-        //*case ENODATA: return code::NoMessages;
-      case ENODEV: return code::DeviceNotFound;
-      case ENOENT: return code::ResourceNotFound;
-        //*case ENOEXEC: return code::ResourceNotExecutable;
+      case ENETUNREACH: return code::network_unavailable;
+      case ENFILE: return code::resource_limit_reached;
+      case ENOBUFS: return code::insufficient_size;
+        //*case ENODATA: return code::no_messages;
+      case ENODEV: return code::device_not_found;
+      case ENOENT: return code::resource_not_found;
+        //*case ENOEXEC: return code::resource_not_executable;
         //**ENOLCK          No locks available (POSIX.1-2001).
         //**ENOLINK         Link has been severed (POSIX.1-2001).
-      case ENOMEM: return code::OutOfMemory;
-        //*case ENOMSG: return code::NoMessages;
+      case ENOMEM: return code::out_of_memory;
+        //*case ENOMSG: return code::no_messages;
         //**ENOPROTOOPT     Protocol not available (POSIX.1-2001).
-      case ENOSPC: return code::DeviceOutOfMemory;
+      case ENOSPC: return code::device_out_of_memory;
         //**ENOSR           No STREAM resources (POSIX.1 (XSI STREAMS option)).
-      case ENOSTR: return code::ResourceUnexpectedType;
-      case ENOSYS: return code::NotImplemented;
-      case ENOTCONN: return code::NotConnected;
-      case ENOTDIR: return code::ResourceUnexpectedType;
+      case ENOSTR: return code::resource_unexpected_type;
+      case ENOSYS: return code::not_implemented;
+      case ENOTCONN: return code::not_connected;
+      case ENOTDIR: return code::resource_unexpected_type;
         //**ENOTEMPTY       Directory not empty (POSIX.1-2001).
-      case ENOTRECOVERABLE: return code::UnrecoverableState;
-      case ENOTSOCK: return code::ResourceUnexpectedType;
-      case ENOTSUP: return code::NotSupported;
-      case ENOTTY: return code::IOError;
-      case ENXIO: return code::BadAddress;
-      case EOVERFLOW: return code::ArithmeticOverflow;
+      case ENOTRECOVERABLE: return code::unrecoverable_state;
+      case ENOTSOCK: return code::resource_unexpected_type;
+      case ENOTSUP: return code::not_supported;
+      case ENOTTY: return code::io_error;
+      case ENXIO: return code::bad_address;
+      case EOVERFLOW: return code::arithmetic_overflow;
         //**EOWNERDEAD      Owner died (POSIX.1-2008).
-      case EPERM: return code::PermissionDenied;
+      case EPERM: return code::permission_denied;
         //**EPIPE           Broken pipe (POSIX.1-2001).
         //**EPROTO          Protocol error (POSIX.1-2001).
         //**EPROTONOSUPPORT Protocol not supported (POSIX.1-2001).
-      case EPROTOTYPE: return code::ResourceUnexpectedType;
-      case ERANGE: return code::OutOfRange;
+      case EPROTOTYPE: return code::resource_unexpected_type;
+      case ERANGE: return code::out_of_range;
         //**ESPIPE          Invalid seek (POSIX.1-2001).
-      case ESRCH: return code::ResourceNotFound;
+      case ESRCH: return code::resource_not_found;
       case ETIME:
-      case ETIMEDOUT: return code::TimedOut;
-      case ETXTBSY: return code::Busy;
+      case ETIMEDOUT: return code::timed_out;
+      case ETXTBSY: return code::busy;
         //**EWOULDBLOCK     Operation would block (may be same value as EAGAIN)
         //**EXDEV           Improper link (POSIX.1-2001).
       default:
-        return code::Unknown;
+        return code::unknown;
     }
   }
 }
@@ -107,135 +107,135 @@ namespace {
 namespace {
   inline code translateWin32CodeToGeneric(const uint32_t dwordValue) noexcept {
     switch (dwordValue) {
-      case 0x1: return code::NotImplemented;
+      case 0x1: return code::not_implemented;
       case 0x2:
-      case 0x3: return code::ResourceNotFound;
-      case 0x4: return code::ResourceLimitReached;
-      case 0x5: return code::PermissionDenied;
-      case 0x6: return code::InvalidResource;
-      case 0x8: return code::OutOfMemory;
-      case 0xb: return code::InvalidFormat;
-      case 0xc: return code::PermissionDenied;
-      case 0xe: return code::OutOfMemory;
-      case 0xf: return code::DeviceNotFound;
+      case 0x3: return code::resource_not_found;
+      case 0x4: return code::resource_limit_reached;
+      case 0x5: return code::permission_denied;
+      case 0x6: return code::invalid_resource;
+      case 0x8: return code::out_of_memory;
+      case 0xb: return code::invalid_format;
+      case 0xc: return code::permission_denied;
+      case 0xe: return code::out_of_memory;
+      case 0xf: return code::device_not_found;
       case 0x10:
       //case 0x11: return EXDEV;
-      case 0x13: return code::PermissionDenied;
-      case 0x14: return code::DeviceNotFound;
-      case 0x15: return code::DeviceBusy;
-      case 0x19: return code::IOError;
+      case 0x13: return code::permission_denied;
+      case 0x14: return code::device_not_found;
+      case 0x15: return code::device_busy;
+      case 0x19: return code::io_error;
       case 0x1d:
-      case 0x1e: return code::DeviceBadTransfer;
-      case 0x1f: return code::ExternalError;
-      case 0x20: return code::PermissionDenied;
+      case 0x1e: return code::device_bad_transfer;
+      case 0x1f: return code::external_error;
+      case 0x20: return code::permission_denied;
       //case 0x21: return ENOLCK;
-      case 0x26: return code::EndOfFile;
-      case 0x27: return code::DeviceOutOfMemory;
-      case 0x37: return code::LostDevice;
-      case 0x38: return code::ResourceLimitReached;
-      case 0x3C: return code::IncompatibleDevice;
-      case 0x41: return code::ConnectionRefused;
+      case 0x26: return code::end_of_file;
+      case 0x27: return code::device_out_of_memory;
+      case 0x37: return code::lost_device;
+      case 0x38: return code::resource_limit_reached;
+      case 0x3C: return code::incompatible_device;
+      case 0x41: return code::connection_refused;
       case 0x44:
-      case 0x45: return code::ResourceLimitReached;
-      case 0x47: return code::ConnectionLimitReached;
-      case 0x50: return code::ResourceAlreadyExists;
-      case 0x52: return code::PermissionDenied;
-      case 0x57: return code::InvalidArgument;
-      case 0x6e: return code::IOError;
-      case 0x6f: return code::ResourceTooLarge;
-      case 0x70: return code::DeviceOutOfMemory;
-      case 0x78: return code::NotImplemented;
-      case 0x79: return code::TimedOut;
-      case 0x7A: return code::InsufficientSize;
+      case 0x45: return code::resource_limit_reached;
+      case 0x47: return code::connection_limit_reached;
+      case 0x50: return code::resource_already_exists;
+      case 0x52: return code::permission_denied;
+      case 0x57: return code::invalid_argument;
+      case 0x6e: return code::io_error;
+      case 0x6f: return code::resource_too_large;
+      case 0x70: return code::device_out_of_memory;
+      case 0x78: return code::not_implemented;
+      case 0x79: return code::timed_out;
+      case 0x7A: return code::insufficient_size;
       case 0x7b:
-      case 0x7c: return code::InvalidArgument;
+      case 0x7c: return code::invalid_argument;
       case 0x7e:
-      case 0x7f: return code::ResourceNotFound;
-      case 0x83: return code::InvalidArgument;
-      case 0x8e: return code::DeviceBusy;
-      //case 0x91: return code::NotEmpty;
-      case 0x94: return code::ResourceUnavailable;
-      case 0x9a: return code::ResourceTooLarge;
-      case 0x9b: return code::ResourceLimitReached;
+      case 0x7f: return code::resource_not_found;
+      case 0x83: return code::invalid_argument;
+      case 0x8e: return code::device_busy;
+      //case 0x91: return code::not_empty;
+      case 0x94: return code::resource_unavailable;
+      case 0x9a: return code::resource_too_large;
+      case 0x9b: return code::resource_limit_reached;
       case 0x9f:
-      case 0xa0: return code::InvalidArgument;
-      case 0xa4: return code::ResourceLimitReached;
-      case 0xaa: return code::Busy;
-      case 0xb7: return code::ResourceAlreadyExists;
-      case 0xbb: return code::ResourceNotFound;
+      case 0xa0: return code::invalid_argument;
+      case 0xa4: return code::resource_limit_reached;
+      case 0xaa: return code::busy;
+      case 0xb7: return code::resource_already_exists;
+      case 0xbb: return code::resource_not_found;
       //case 0xd4: return ENOLCK;
-      case 0x10b: return code::InvalidArgument;
-      case 0x120: return code::ResourceNotOwned;
-      case 0x12b: return code::FurtherActionsRequired;
-      case 0x1e7: return code::BadAddress;
-      case 0x216: return code::ArithmeticOverflow;
-      case 0x217: return code::AlreadyConnected;
-      case 0x225: return code::BadAlignment;
-      case 0x23b: return code::PermissionDenied;
-      case 0x23e: return code::UnhandledError;
-      case 0x23f: return code::InitializationFailure;
+      case 0x10b: return code::invalid_argument;
+      case 0x120: return code::resource_not_owned;
+      case 0x12b: return code::further_actions_required;
+      case 0x1e7: return code::bad_address;
+      case 0x216: return code::arithmetic_overflow;
+      case 0x217: return code::already_connected;
+      case 0x225: return code::bad_alignment;
+      case 0x23b: return code::permission_denied;
+      case 0x23e: return code::unhandled_error;
+      case 0x23f: return code::initialization_failure;
       case 0x246:
-      case 0x247: return code::InvalidFormat;
-      case 0x257: return code::StackOverflow;
-      case 0x25d: return code::DataCorruption;
-      case 0x287: return code::InitializationFailure;
-      case 0x29c: return code::FailedAssertion;
-      case 0x2b0: return code::UnhandledError;
-      case 0x2c6: return code::Done;
-      case 0x2c7: return code::Waiting;
-      case 0x30b: return code::DataCorruption;
-      case 0x3e3: return code::Cancelled;
-      case 0x3e6: return code::PermissionDenied;
-      case 0x3e9: return code::StackOverflow;
-      case 0x3ec: return code::InvalidArgument;
+      case 0x247: return code::invalid_format;
+      case 0x257: return code::stack_overflow;
+      case 0x25d: return code::data_corruption;
+      case 0x287: return code::initialization_failure;
+      case 0x29c: return code::failed_assertion;
+      case 0x2b0: return code::unhandled_error;
+      case 0x2c6: return code::done;
+      case 0x2c7: return code::waiting;
+      case 0x30b: return code::data_corruption;
+      case 0x3e3: return code::cancelled;
+      case 0x3e6: return code::permission_denied;
+      case 0x3e9: return code::stack_overflow;
+      case 0x3ec: return code::invalid_argument;
       //case 0x3f3:
       //case 0x3f4:
       //case 0x3f5: return EIO;
-      case 0x4c7: return code::Cancelled;
-      case 0x4cd: return code::NotConnected;
-      case 0x4ce: return code::AlreadyConnected;
-      case 0x4cf: return code::NetworkUnavailable;
+      case 0x4c7: return code::cancelled;
+      case 0x4cd: return code::not_connected;
+      case 0x4ce: return code::already_connected;
+      case 0x4cf: return code::network_unavailable;
       case 0x4d3:
-      case 0x4d4: return code::ConnectionAborted;
-      case 0x4d5: return code::Busy;
-      case 0x4d6: return code::ConnectionLimitReached;
-      case 0x7d0: return code::InvalidFormat;
+      case 0x4d4: return code::connection_aborted;
+      case 0x4d5: return code::busy;
+      case 0x4d6: return code::connection_limit_reached;
+      case 0x7d0: return code::invalid_format;
       case 0x961:
-      case 0x964: return code::DeviceBusy;
-      case 0x2714: return code::Interrupted;
-      case 0x2719: return code::ResourceNotFound;
-      case 0x271d: return code::PermissionDenied;
-      case 0x271e: return code::BadAddress;
-      case 0x2726: return code::InvalidArgument;
-      case 0x2728: return code::ResourceLimitReached;
-      case 0x2733: return code::Busy;
+      case 0x964: return code::device_busy;
+      case 0x2714: return code::interrupted;
+      case 0x2719: return code::resource_not_found;
+      case 0x271d: return code::permission_denied;
+      case 0x271e: return code::bad_address;
+      case 0x2726: return code::invalid_argument;
+      case 0x2728: return code::resource_limit_reached;
+      case 0x2733: return code::busy;
       case 0x2734:
-      case 0x2735: return code::InProgress;
-      case 0x2736: return code::ResourceUnexpectedType;
+      case 0x2735: return code::in_progress;
+      case 0x2736: return code::resource_unexpected_type;
       //case 0x2737: return EDESTADDRREQ;
-      case 0x2738: return code::ResourceTooLarge;
+      case 0x2738: return code::resource_too_large;
       //case 0x2739: return EPROTOTYPE;
       //case 0x273a: return ENOPROTOOPT;
       case 0x273b:
       case 0x273d:
-      case 0x273f: return code::NotSupported;
+      case 0x273f: return code::not_supported;
       case 0x2740:
-      case 0x2741: return code::AddressNotAvailable;
+      case 0x2741: return code::address_not_available;
       case 0x2742:
       case 0x2743:
-      case 0x2744: return code::NetworkUnavailable;
-      case 0x2745: return code::ConnectionAborted;
-      case 0x2746: return code::ConnectionReset;
-      case 0x2747: return code::InsufficientSize;
-      case 0x2748: return code::AlreadyConnected;
-      case 0x2749: return code::NotConnected;
-      case 0x274c: return code::TimedOut;
-      case 0x274d: return code::ConnectionRefused;
-      case 0x274f: return code::ResourceTooLarge;
+      case 0x2744: return code::network_unavailable;
+      case 0x2745: return code::connection_aborted;
+      case 0x2746: return code::connection_reset;
+      case 0x2747: return code::insufficient_size;
+      case 0x2748: return code::already_connected;
+      case 0x2749: return code::not_connected;
+      case 0x274c: return code::timed_out;
+      case 0x274d: return code::connection_refused;
+      case 0x274f: return code::resource_too_large;
       //case 0x2751: return EHOSTUNREACH;
-      case 0x3BC3: return code::DeviceNotFound;
+      case 0x3BC3: return code::device_not_found;
       default:
-        return code::Unknown;
+        return code::unknown;
     }
   }
   inline int translateWin32StatusToPosixCode(const uint32_t dwordValue) noexcept {
