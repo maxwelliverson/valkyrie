788f5a6edfaa868ab0aba69111682300dd2a21ad
Maxwell Iverson <maxwelliverson@gmail.com>
2021-05-06 13:37:58 -0700

    `<valkyrie/async/atomic.hpp>` refactoring, combine function declarations and instead dispatch via `if constexpr` statements

--- include/valkyrie/async/atomic.hpp
+++ include/valkyrie/async/atomic.hpp
@@ -29,6 +29,14 @@ namespace valkyrie{
     struct get_atomic_type<T&>{
       using type = std::atomic_ref<T>;
     };
+
+    template <typename T>
+    struct is_atomic : meta::false_type {};
+    template <typename T>
+    struct is_atomic<std::atomic<T>> : meta::true_type{};
+
+    template <typename T>
+    VK_constant bool is_atomic_v = is_atomic<T>::value;
   }
 
   template <typename T>
@@ -50,116 +58,116 @@ namespace valkyrie{
 #define VK_using_enum using namespace
 #endif
 
-
-
-
-
-
   template <std::memory_order Order = std::memory_order_acq_rel, typename T>
-  inline static bool atomic_cas(std::atomic<T>& location, T& expected, param_t<identity_type_t<T>> value) noexcept {
-    return location.compare_exchange_strong(expected, value, Order);
+  inline static bool atomic_cas(T& location, T& expected, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.compare_exchange_strong(expected, value, Order);
+    }
+    else {
+      return atomic<T&>(location).compare_exchange_strong(expected, value, Order);
+    }
   }
   template <std::memory_order Order = std::memory_order_release, typename T>
-  inline static T    atomic_exchange(std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    return location.exchange(value, Order);
+  inline static T    atomic_exchange(T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.exchange(value, Order);
+    }
+    else {
+      return atomic<T&>(location).exchange(value, Order);
+    }
   }
   template <std::memory_order Order = std::memory_order_release, typename T>
-  inline static void atomic_store(std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    location.store(value, Order);
-  }
-  template <std::memory_order Order = std::memory_order_acquire, typename T>
-  inline static T    atomic_load(const std::atomic<T>& location) noexcept {
-    return location.load(Order);
-  }
+  inline static void atomic_store(T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      location.store(value, Order);
+    }
+    else {
+      atomic<T&>(location).store(value, Order);
+    }
 
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires numeric_type<T>
-  inline static T    atomic_fetch_add(std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    return location.fetch_add(value, Order);
-  }
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires numeric_type<T>
-  inline static T    atomic_fetch_sub(std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    return location.fetch_sub(value, Order);
-  }
-
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_type<T>
-  inline static T    atomic_fetch_and(std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    return location.fetch_and(value, Order);
-  }
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_type<T>
-  inline static T    atomic_fetch_or(std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    return location.fetch_or(value, Order);
-  }
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_type<T>
-  inline static T    atomic_fetch_xor(std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    return location.fetch_xor(value, Order);
-  }
-
-  template <std::memory_order Order = std::memory_order_acquire, typename T>
-  inline static void atomic_wait(const std::atomic<T>& location, param_t<identity_type_t<T>> value) noexcept {
-    atomic<T&>(location).wait(value, Order);
-  }
-  template <typename T>
-  inline static void atomic_notify_one(std::atomic<T>& location) noexcept {
-    atomic<T&>(location).notify_one();
-  }
-  template <typename T>
-  inline static void atomic_notify_all(std::atomic<T>& location) noexcept {
-    atomic<T&>(location).notify_all();
-  }
-
-
-
-
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T>
-  inline static bool atomic_cas(T& location, T& expected, param_t<identity_type_t<T>> value) noexcept {
-    return atomic<T&>(location)
-        .compare_exchange_strong(expected, value, Order);
-  }
-  template <std::memory_order Order = std::memory_order_release, typename T>
-  inline static T    atomic_exchange(T& location, param_t<identity_type_t<T>> value) noexcept {
-    return atomic<T&>(location).exchange(value, Order);
-  }
-  template <std::memory_order Order = std::memory_order_release, typename T>
-  inline static void atomic_store(T& location, param_t<identity_type_t<T>> value) noexcept {
-    atomic<T&>(location).store(value, Order);
   }
   template <std::memory_order Order = std::memory_order_acquire, typename T>
   inline static T    atomic_load(const T& location) noexcept {
-    return atomic<T&>(const_cast<T&>(location)).load(Order);
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.load(Order);
+    }
+    else {
+      return atomic<T&>(const_cast<T&>(location)).load(Order);
+    }
   }
 
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires numeric_type<T>
-  inline static T    atomic_fetch_add(T& location, param_t<identity_type_t<T>> value) noexcept {
-    return atomic<T&>(location).fetch_add(value, Order);
+  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires numeric_c<T>
+  inline static T    atomic_fetch_add(T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.fetch_add(value, Order);
+    }
+    else {
+      return atomic<T&>(location).fetch_add(value, Order);
+    }
   }
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires numeric_type<T>
-  inline static T    atomic_fetch_sub(T& location, param_t<identity_type_t<T>> value) noexcept {
-    return atomic<T&>(location).fetch_sub(value, Order);
+  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires numeric_c<T>
+  inline static T    atomic_fetch_sub(T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.fetch_sub(value, Order);
+    }
+    else {
+      return atomic<T&>(location).fetch_sub(value, Order);
+    }
   }
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_type<T>
-  inline static T    atomic_fetch_and(T& location, param_t<identity_type_t<T>> value) noexcept {
-    return atomic<T&>(location).fetch_and(value, Order);
+  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_c<T>
+  inline static T    atomic_fetch_and(T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.fetch_and(value, Order);
+    }
+    else {
+      return atomic<T&>(location).fetch_and(value, Order);
+    }
   }
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_type<T>
-  inline static T    atomic_fetch_or(T& location, param_t<identity_type_t<T>> value) noexcept {
-    return atomic<T&>(location).fetch_or(value, Order);
+  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_c<T>
+  inline static T    atomic_fetch_or(T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.fetch_or(value, Order);
+    }
+    else {
+      return atomic<T&>(location).fetch_or(value, Order);
+    }
   }
-  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_type<T>
-  inline static T    atomic_fetch_xor(T& location, param_t<identity_type_t<T>> value) noexcept {
-    return atomic<T&>(location).fetch_xor(value, Order);
+  template <std::memory_order Order = std::memory_order_acq_rel, typename T> requires integral_c<T>
+  inline static T    atomic_fetch_xor(T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      return location.fetch_xor(value, Order);
+    }
+    else {
+      return atomic<T&>(location).fetch_xor(value, Order);
+    }
   }
 
   template <std::memory_order Order = std::memory_order_acquire, typename T>
-  inline static void atomic_wait(const T& location, param_t<identity_type_t<T>> value) noexcept {
-    atomic<T&>(const_cast<T&>(location)).wait(value, Order);
+  inline static void atomic_wait(const T& location, identity_type_t<T> value) noexcept {
+    if constexpr ( detail::is_atomic_v<T> ) {
+      location.wait(value, Order);
+    }
+    else {
+      atomic<T&>(const_cast<T&>(location)).wait(value, Order);
+    }
   }
   template <typename T>
   inline static void atomic_notify_one(T& location) noexcept {
-    atomic<T&>(location).notify_one();
+    if constexpr ( detail::is_atomic_v<T> ) {
+      location.notify_one();
+    }
+    else {
+      atomic<T&>(location).notify_one();
+    }
   }
   template <typename T>
   inline static void atomic_notify_all(T& location) noexcept {
-    atomic<T&>(location).notify_all();
+    if constexpr ( detail::is_atomic_v<T> ) {
+      location.notify_all();
+    }
+    else {
+      atomic<T&>(location).notify_all();
+    }
   }
 }
 
